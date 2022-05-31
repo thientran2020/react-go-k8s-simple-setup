eb441521fd172870c91aa084e81f0395805c178a
thientran <thien.y.tran@sjsu.edu>
2022-05-30 17:29:05 -0700

    Initial commits

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:latest
+
+ENV GO111MODULE=on
+
+WORKDIR /app
+
+COPY ./go.mod .
+
+RUN go mod download
+
+COPY . .
+
+CMD ["go", "run", "main.go"]
+
---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			ping: "Waiting"
+		}
+	}
+
+	conponentDidMount() {
+		axios.get('/api/ping')
+			.then((res, err) => {
+				if (res.status === 200) {
+					this.setState({
+						ping: res.data.message
+					})
+				} else {
+					console.log(err)
+				}
+			})
+	}
+ 
+	render() {
+		return (
+		  <div className="App">
+			<header className="App-header">
+			  <img src={logo} className="App-logo" alt="logo" />
+			  <a
+				className="App-link"
+				target="_blank"
+				rel="noopener noreferrer"
+			  >
+				{this.state.ping}
+			  </a>
+			</header>
+		  </div>
+		);
+	}
+}
+
+export default App;
